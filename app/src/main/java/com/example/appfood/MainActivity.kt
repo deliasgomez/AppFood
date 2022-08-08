f6cc78c5284b22cb038138b37455044a27aa294e
Daniel Elías Torres Gómez <doctorresg@gmail.com>
2022-08-08 10:17:00 -0500

    initial commit

---
+++ app/src/main/java/com/example/appfood/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.example.appfood
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.navigation.Navigation
+import androidx.navigation.ui.NavigationUI
+import com.example.appfood.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val navController = Navigation.findNavController(this,R.id.host_fragment)
+        NavigationUI.setupWithNavController(binding.btnNav,navController)
+    }
+}
